8112842e421bef5b56c1fcc9b24f9710e7ead81e
Ayush Likhar <ayush.likhar@outlook.com>
2019-11-06 09:28:26 +0530

    part2

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ Vue.use(VueRouter)
 import App from './views/App'
 import Hello from './views/Hello'
 import Home from './views/Home'
+import UsersIndex from './views/UsersIndex'
 
 const router = new VueRouter({
     mode: 'history',
@@ -20,6 +21,11 @@ const router = new VueRouter({
             path: '/hello',
             name: 'hello',
             component: Hello,
+        },
+        {
+            path: '/users',
+            name: 'users.index',
+            component: UsersIndex,
         },
     ],
 });
